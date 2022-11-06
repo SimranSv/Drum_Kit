b90c2146d8620e401897bcd3783409c0f4674f0b
Simran Vedpathak <80968391+SimranSv@users.noreply.github.com>
2022-11-07 01:37:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+document.querySelectorAll(".drum")[0].addEventListener("click", fun0);
+function fun0() {
+    //  alert("LOL");
+    var audio = new Audio('sounds/crash.mp3');
+    audio.play();
+}
+document.querySelectorAll(".drum")[1].addEventListener("click", fun1);
+function fun1() {
+    //  alert("LOL");
+    var audio = new Audio('sounds/kick-bass.mp3');
+    audio.play();
+}
+document.querySelectorAll(".drum")[2].addEventListener("click", fun2);
+function fun2() {
+    //  alert("LOL");
+    var audio = new Audio('sounds/snare.mp3');
+    audio.play();
+}
+document.querySelectorAll(".drum")[3].addEventListener("click", fun3);
+function fun3() {
+    //  alert("LOL");
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+}
+document.querySelectorAll(".drum")[4].addEventListener("click", fun4);
+function fun4() {
+    //  alert("LOL");
+    var audio = new Audio('sounds/tom-2.mp3');
+    audio.play();
+}
+
+document.querySelectorAll(".drum")[5].addEventListener("click", fun5);
+function fun5() {
+    //  alert("LOL");
+    var audio = new Audio('sounds/tom-3.mp3');
+    audio.play();
+}
+document.querySelectorAll(".drum")[6].addEventListener("click", fun6);
+function fun6() {
+    //  alert("LOL");
+    var audio = new Audio('sounds/tom-4.mp3');
+    audio.play();
+}
